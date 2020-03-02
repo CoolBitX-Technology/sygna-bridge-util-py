a50815ef1a6cbac36d8b2bec29cf9d0c917691e5
kunming.liu <kunming@coolbitx.com>
2020-03-03 02:26:12 +0800

    modified sign_data and added test for sign

--- crypto/main.py
+++ crypto/main.py
@@ -1,13 +1,13 @@
 from . import ecies, sign as sygna_sign
-from api import (
-    check_sign_callback_parameters,
-    check_sign_permission_parameters,
-    check_sign_permission_request_parameters,
-    check_sign_tx_id_parameters,
-    PermissionStatus
-)
 import json
-from typing import Union
+from validator import (
+    validate_permission_schema,
+    validate_permission_request_schema,
+    validate_txid_schema,
+    validate_callback_schema,
+    validate_private_key
+)
+from config import PermissionStatus
 
 
 def sygna_encode_private_data(data: dict, public_key: str) -> str:
@@ -38,44 +38,51 @@ def sign_data(data: dict, private_key: str) -> dict:
     Returns:
         dict. original object adding a signature field
     """
-    data_str = json.dumps(data)
-    signature = sygna_sign.sign_message(data_str, private_key)
-    data['signature'] = signature.encode("hex")
+    data['signature'] = ''
+    signature = sygna_sign.sign_message(data, private_key)
+    print(f'sign_data signature ={signature}')
+    data['signature'] = signature
     return data
 
 
-def sign_permission_request(private_info: str, transaction: dict, data_dt: str, private_key: str) -> dict:
+def sign_permission_request(data: dict, private_key: str) -> dict:
     """ sign permission request data
 
     Args:
-        private_info (str)
-        transaction (dict)
-        data_dt (str)
+        data(dict):{
+            private_info (str)
+            transaction (dict)
+            data_dt (str)
+            Optional expire_date(int)
+        }
         private_key (str)
 
     Returns:
-        dict({private_info: str, transaction:dict, data_dt:str, signature:str})
+        dict({private_info: str, transaction:dict, data_dt:str, expire_date?:int, signature:str})
 
     Raises:
         Exception('parameters are not valid')
     """
-    check_sign_permission_request_parameters(
-        private_info, transaction, data_dt, private_key)
+    validate_permission_request_schema(data)
+    validate_private_key(private_key)
 
-    data = {
-        'private_info': private_info,
-        'transaction': transaction,
-        'data_dt': data_dt
+    data_to_sign = {
+        'private_info': data['private_info'],
+        'transaction': data['transaction'],
+        'data_dt': data['data_dt']
     }
-    result = sign_data(data, private_key)
-    return result
+    if 'expire_date' in data:
+        data_to_sign['expire_date'] = data['expire_date']
+    return sign_data(data_to_sign, private_key)
 
 
-def sign_callback(callback_url: str, private_key: str) -> dict:
+def sign_callback(data: dict, private_key: str) -> dict:
     """ sign callback data
 
     Args:
-        callback_url (str)
+        data (dict): {
+            callback_url: str
+        }
         private_key (str)
 
     Returns:
@@ -84,46 +91,63 @@ def sign_callback(callback_url: str, private_key: str) -> dict:
     Raises:
         Exception('parameters are not valid')
     """
-    check_sign_callback_parameters(callback_url, private_key)
-    data = {
-        'callback_url': callback_url
+    validate_callback_schema(data)
+    validate_private_key(private_key)
+    data_to_sign = {
+        'callback_url': data['callback_url']
     }
-    result = sign_data(data, private_key)
-    return result
+    return sign_data(data_to_sign, private_key)
 
 
-def sign_permission(transfer_id: str, permission_status: Union[PermissionStatus, str], private_key: str) -> dict:
+def sign_permission(data: dict, private_key: str) -> dict:
     """ sign permission data
 
     Args:
-        transfer_id (str)
-        permission_status (PermissionStatus)
+        data(dict):{
+            transfer_id (str)
+            permission_status (str) : ACCEPTED or REJECTED
+            Optional expire_date(int)
+            Optional reject_code(str) : BVRC001,BVRC002,BVRC003,BVRC004 or BVRC999
+            Optional reject_message(str):
+                unsupported_currency,
+                service_downtime,
+                exceed_trading_volume,
+                compliance_check_fail or other
+        }
         private_key (str)
 
     Returns:
-        dict({transfer_id:str, permission_status: str, signature: str})
+        dict({transfer_id:str, permission_status: str,expire_date?:int,reject_code?:str,reject_message?:str , signature: str})
 
     Raises:
         Exception('parameters are not valid')
     """
+    validate_permission_schema(data)
+    validate_private_key(private_key)
 
-    check_sign_permission_parameters(
-        transfer_id, permission_status, private_key)
-
-    data = {
-        'transfer_id': transfer_id,
-        'permission_status': permission_status.value
+    data_to_sign = {
+        'transfer_id': data['transfer_id'],
+        'permission_status': data['permission_status'],
     }
-    result = sign_data(data, private_key)
-    return result
+
+    if 'expire_date' in data:
+        data_to_sign['expire_date'] = data['expire_date']
+
+    if data['permission_status'] == PermissionStatus.REJECTED.value:
+        data_to_sign['reject_code'] = data['reject_code']
+        if 'reject_message' in data:
+            data_to_sign['reject_message'] = data['reject_message']
+    return sign_data(data_to_sign, private_key)
 
 
-def sign_txid(transfer_id: str, txid: str, private_key: str) -> dict:
+def sign_txid(data: dict, private_key: str) -> dict:
     """ sign txid data
 
     Args:
-        transfer_id (str)
-        txid (str)
+        data(dict):{
+            transfer_id (str)
+            txid (str)
+        }
         private_key (str)
 
     Returns:
@@ -133,12 +157,11 @@ def sign_txid(transfer_id: str, txid: str, private_key: str) -> dict:
         Exception('parameters are not valid')
     """
 
-    check_sign_tx_id_parameters(
-        transfer_id, txid, private_key)
+    validate_txid_schema(data)
+    validate_private_key(private_key)
 
-    data = {
-        'transfer_id': transfer_id,
-        'txid': txid
+    data_to_sign = {
+        'transfer_id': data['transfer_id'],
+        'txid': data['txid']
     }
-    result = sign_data(data, private_key)
-    return result
+    return sign_data(data_to_sign, private_key)
