77426ae1cc2f7d1d7cb8e35bef8504c247fbead1
kunming.liu <kunming@coolbitx.com>
2020-03-03 00:02:12 +0800

    use jsonschema to validate json schema

---
+++ validator/__init__.py
@@ -0,0 +1,29 @@
+from .validateschema import (
+    validate_schema,
+    validate_permission_schema,
+    validate_permission_request_schema,
+    validate_txid_schema,
+    validate_callback_schema,
+    validate_post_permission_schema,
+    validate_post_permission_request_schema,
+    validate_post_txid_schema
+)
+from .validatedata import (
+    validate_private_key,
+    validate_transfer_id,
+    validate_expire_date
+)
+
+__ALL__ = [
+    'validate_schema',
+    'validate_permission_schema',
+    'validate_permission_request_schema',
+    'validate_txid_schema',
+    'validate_callback_schema',
+    'validate_post_permission_schema',
+    'validate_post_permission_request_schema',
+    'validate_post_txid_schema',
+    'validate_private_key',
+    'validate_transfer_id',
+    'validate_expire_date'
+]
