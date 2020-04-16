2578c3fb418c3ef41b5537b52404aed59674308d
kunming.liu <kunming@coolbitx.com>
2020-04-16 22:53:55 +0800

    revise version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name='sygna-bridge-util',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=get_requirements(),
