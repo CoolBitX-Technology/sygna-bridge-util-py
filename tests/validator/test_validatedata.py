1ee641562dc84c34a7a360a9fa21c006a42eaf2a
kunming.liu <kunming@coolbitx.com>
2020-05-23 16:05:05 +0800

    delete useless functions

--- tests/validator/test_validatedata.py
+++ tests/validator/test_validatedata.py
@@ -1,7 +1,5 @@
 import pytest
-from sygna_bridge_util.validator import validate_private_key, validate_transfer_id, validate_expire_date
-from sygna_bridge_util.config import EXPIRE_DATE_MIN_OFFSET
-from freezegun import freeze_time
+from sygna_bridge_util.validator import validate_private_key, validate_transfer_id
 
 
 def test_validate_private_key():
@@ -56,45 +54,6 @@ def test_validate_transfer_id():
         pytest.fail("Unexpected TypeError or ValueError")
 
 
-def test_validate_expire_date():
-    """should raise exception if expire_date is not valid"""
-    with pytest.raises(TypeError) as excinfo:
-        validate_expire_date('123')
-    assert "Expect expire_date to be <class 'int'>, got <class 'str'>" == str(
-        excinfo.value)
-
-    with pytest.raises(TypeError) as excinfo:
-        validate_expire_date({'key': 'value'})
-    assert "Expect expire_date to be <class 'int'>, got <class 'dict'>" == str(
-        excinfo.value)
-
-    freezer = freeze_time("2020-02-28 09:30:28")
-    freezer.start()
-    with pytest.raises(ValueError) as excinfo:
-        validate_expire_date(1582885828)  # GMT 2020-02-28 10:30:28
-    assert "expire_date should be at least {0} seconds away from the current time.".format(
-        EXPIRE_DATE_MIN_OFFSET / 1000) == str(excinfo.value)
-
-    with pytest.raises(ValueError) as excinfo:
-        validate_expire_date(1582882365000)  # GMT 2020-02-28 09:32:45:000
-    assert "expire_date should be at least {0} seconds away from the current time.".format(
-        EXPIRE_DATE_MIN_OFFSET / 1000) == str(excinfo.value)
-
-    with pytest.raises(ValueError) as excinfo:
-        validate_expire_date(1582876947000)  # GMT 2020-02-28 08:02:27:000
-    assert "expire_date should be at least {0} seconds away from the current time.".format(
-        EXPIRE_DATE_MIN_OFFSET / 1000) == str(excinfo.value)
-
-    try:
-        validate_expire_date(1582885828000)  # GMT 2020-02-28 10:30:28:000
-        validate_expire_date(1582882408000)  # GMT 2020-02-28 09:33:28:000
-    except (TypeError, ValueError):
-        pytest.fail("Unexpected TypeError or ValueError")
-
-    freezer.stop()
-
-
 if __name__ == '__main__':
     test_validate_private_key()
     test_validate_transfer_id()
-    test_validate_expire_date()
