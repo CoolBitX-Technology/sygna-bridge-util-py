48e005ba2c7afb1faff5734444036f7b763a26f2
kunming.liu <kunming@coolbitx.com>
2020-07-21 11:33:37 +0800

    delete useless function

--- src/sygna_bridge_util/validator/validatedata.py
+++ src/sygna_bridge_util/validator/validatedata.py
@@ -8,15 +8,3 @@ def validate_private_key(private_key: str) -> None:
 
     if len(private_key) < 1:
         raise ValueError('private_key is too short')
-
-
-def validate_transfer_id(transfer_id: str) -> None:
-    if type(transfer_id) is not str:
-        raise TypeError('Expect {0} to be {1}, got {2}'.format(
-            'transfer_id',
-            str,
-            type(transfer_id))
-        )
-
-    if len(transfer_id) != 64:
-        raise ValueError('transfer_id length should be 64')
--- tests/validator/test_validatedata.py
+++ tests/validator/test_validatedata.py
@@ -1,5 +1,5 @@
 import pytest
-from sygna_bridge_util.validator import validate_private_key, validate_transfer_id
+from sygna_bridge_util.validator import validate_private_key
 
 
 def test_validate_private_key():
@@ -26,34 +26,5 @@ def test_validate_private_key():
         pytest.fail("Unexpected TypeError or ValueError")
 
 
-def test_validate_transfer_id():
-    """should raise exception if transfer_id is not valid"""
-    with pytest.raises(TypeError) as excinfo:
-        validate_transfer_id(123)
-    assert "Expect transfer_id to be <class 'str'>, got <class 'int'>" == str(
-        excinfo.value)
-
-    with pytest.raises(TypeError) as excinfo:
-        validate_transfer_id({'key': 'value'})
-    assert "Expect transfer_id to be <class 'str'>, got <class 'dict'>" == str(
-        excinfo.value)
-
-    with pytest.raises(ValueError) as excinfo:
-        validate_transfer_id('')
-    assert "transfer_id length should be 64" == str(
-        excinfo.value)
-
-    with pytest.raises(ValueError) as excinfo:
-        validate_transfer_id('6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b1')  # len =65
-    assert "transfer_id length should be 64" == str(
-        excinfo.value)
-
-    try:
-        validate_transfer_id('6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b')  # len =64
-    except (TypeError, ValueError):
-        pytest.fail("Unexpected TypeError or ValueError")
-
-
 if __name__ == '__main__':
     test_validate_private_key()
-    test_validate_transfer_id()
